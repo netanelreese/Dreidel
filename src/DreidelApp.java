99eb4687b95cfbbdaf7987fb99e22a62fcb9e14f
Nathanael <ngreese7@ou.edu>
2020-12-27 05:23:21 -0600

    Created JFrame for Dreidel Application

--- src/DreidelApp.java
+++ src/DreidelApp.java
@@ -1,9 +1,16 @@
+import javax.swing.JFrame;
 
 public class DreidelApp {
-
+	private String TITLE = "Dreidel";
+	private static JFrame app;
+	
+	public static void application() {
+		app = new JFrame();
+		app.setVisible(true);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		application();
 	}
 
 }
