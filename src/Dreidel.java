b41265a29cf4b9baae904ce0436a317e77858c4d
Nathanael <ngreese7@ou.edu>
2020-12-27 05:17:51 -0600

    Stuck on how to do a different count of players

--- src/Dreidel.java
+++ src/Dreidel.java
@@ -1,4 +1,13 @@
+import java.util.HashMap;
 
 public class Dreidel {
-
+	private int potSize;
+	private int playerCount;
+	private HashMap<Integer, Player> players = new HashMap<Integer, Player>();
+	
+	public Dreidel(int potSize, int playerCount) {
+		this.potSize = potSize;
+		this.playerCount = playerCount;
+	}
+	private void CreatePlayers
 }
