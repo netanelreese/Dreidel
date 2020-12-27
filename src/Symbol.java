7e27bcc789c21eb213e9e1a56dbd0c202f94976c
Nathanael <ngreese7@ou.edu>
2020-12-27 04:59:13 -0600

    Forgot to add type modifier to toString method

--- src/Symbol.java
+++ src/Symbol.java
@@ -1,4 +1,13 @@
 
-public class Symbol {
-
+public enum Symbol {
+	Gimel("Gimel"), Hey("Hey"), Shin("Shin"), Nun("Nun");
+	
+	private String symbol;
+	
+	private Symbol(String symbol) {
+		this.symbol = symbol;
+	}
+	private String toString() {
+		return symbol;
+	}
 }
--- src/Top.java
+++ src/Top.java
@@ -1,4 +1,5 @@
 
 public class Top {
-
+	private DreidelValues
+	
 }
