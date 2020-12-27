cc51c5ed6d99c3cbc7989291290f69f3cdb34f56
Nathanael <ngreese7@ou.edu>
2020-12-27 05:35:03 -0600

    Created game jframe and changed app to splashApp to denote it's the initial app to be shown

--- src/DreidelApp.java
+++ src/DreidelApp.java
@@ -1,21 +1,46 @@
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class DreidelApp {
-	private String TITLE = "Dreidel";
-	private static JFrame app;
+	private static String TITLE = "Dreidel";
+	private static JFrame splashApp;
+	private static JFrame gameApp;
+	private static String START = "Start";
 	
-	public static void application() {
-		app = new JFrame(); //initializing frame
+	public static void initialApp() {
+		int potSize;
+		int playerCount;
+		
+		splashApp = new JFrame(TITLE); //initializing frame
 		
 		ImageIcon dreidelImg = new ImageIcon("dreidel.png"); //creating image icon object
-		app.setIconImage(dreidelImg.getImage()); //setting it to the jframe
+		splashApp.setIconImage(dreidelImg.getImage()); //setting it to the jframe
 		
-		app.setVisible(true);
+		JButton startButton = new JButton(START);
+		splashApp.add(startButton);
+		
+		splashApp.setVisible(true); //making JFrame Visible
+	}
+	public static void gameApp() {
+		int potSize;
+		int playerCount;
+		
+		gameApp = new JFrame(); //initializing frame
+		
+		ImageIcon dreidelImg = new ImageIcon("dreidel.png"); //creating image icon object
+		gameApp.setIconImage(dreidelImg.getImage()); //setting it to the jframe
+		
+		
+		
+		gameApp.setVisible(true); //making JFrame Visible
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		application();
+		initialApp(); //creating application
+		if (splashApp.isActive() == false) {
+			gameApp();
+		}
 	}
 
 }
