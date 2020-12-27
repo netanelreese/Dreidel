c632c214c46d5d89653ab28d5eb3781cb35f4ffa
Nathanael <ngreese7@ou.edu>
2020-12-27 05:11:27 -0600

    Created Player Class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,17 @@
 
 public class Player {
-
+	private int wallet;
+	
+	public Player(int wallet) {
+		this.wallet = wallet;
+	}
+	public int getWallet() {
+		return wallet;
+	}
+	public void decrementWallet() {
+		wallet--;
+	}
+	public void incrementWallet() {
+		wallet++;
+	}
 }
